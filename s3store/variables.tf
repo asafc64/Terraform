a315201db5643253c1b9f2b19032546a265fbfb7
asafc64 <asafc64@gmail.com>
2019-04-28 15:37:09 +0300

    Update variables.tf

--- s3store/variables.tf
+++ s3store/variables.tf
@@ -3,6 +3,7 @@ variable "SANDBOX_ID" {}
 variable "item1" {}
 variable "item2" {}
 variable "item3" {}
+variable "PUBLIC_ADDRESS" {}
 variable "AWS_ACCESS_KEY_ID" {}
 variable "AWS_SECRET_ACCESS_KEY" {}
 variable "AWS_DEFAULT_REGION" {}
