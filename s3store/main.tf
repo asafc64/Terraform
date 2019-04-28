e2fefb4376e30bc04730081c9e302af88a187066
asafc64 <asafc64@gmail.com>
2019-04-28 16:02:26 +0300

    Update main.tf

--- s3store/main.tf
+++ s3store/main.tf
@@ -6,5 +6,5 @@ provider "aws" {
 resource "aws_s3_bucket_object" "object" {
   bucket  = "${var.bucket}"
   key     = "${var.SANDBOX_ID}.json"
-  content = "[\"${var.PUBLIC_ADDRESS}\", \"${var.item1}\", \"${var.item2}\", \"${var.item3}\"]"
+  content = "[\"${var.item1}\", \"${var.item2}\", \"${var.item3}\"]"
 }
